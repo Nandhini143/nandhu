48db9e1ba792fefd0c3153dd23b159fae3e4be84
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-03-20 12:24:44 +0530

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int num,count=0;
+	int n,count=0;
 	cout<<"Enter the digits of number\n";
-	cin>>num;
-	cout<<"Entered number:"<<num<<"\n";
-	while(num!=0)
-	{	num=num/10;
+	cin>>n;
+	cout<<"Entered number:"<<n<<"\n";
+	while(n!=0)
+	{	n=n/10;
 		count+=1;
 	}
 	cout<<count;
