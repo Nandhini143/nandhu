fe7db60ed617ef032abd431a316133cf68bd1968
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-03-22 15:59:48 +0530

    Create 28.cpp

---
+++ 28.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int  main(){
+int i,a[100],n;
+cout<<"enetr the number";
+cin>>n;
+for(i=0;i<n;i++)
+{
+    cin>>a[i];
+}
+for(i=0;i<n;i++)
+{
+cout<<a[i]<<"\t"<<i<<"\n";
+}}
