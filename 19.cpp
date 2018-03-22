cee6a684811b40a0985124840fe8a70f4a927035
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-03-22 15:16:09 +0530

    Create 19.cpp

---
+++ 19.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()  {
+	int i,num,fact=1;
+	cout<<"Enter a number\n";
+	cin>>num;
+	for(i=1;i<=num;i++)
+	{
+		fact*=i;
+	}
+	cout<<fact;
+	return 0;
+}
