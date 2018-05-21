e4f05a0a470cc9a0513651180bdd1163d797c4cf
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-21 11:50:59 +0530

    Create 39.cpp

---
+++ 39.cpp
@@ -0,0 +1,18 @@
+#include <stream>
+  int main() {
+    int a[10];
+    int i;
+    int greatest;
+gets(a[10];
+  
+    for (i = 0; i < 10; i++) {  
+    greatest = a[0];}
+    for (i = 0; i < 10; i++) {
+if (a[i] > greatest) {
+greatest = a[i];
+    }
+    }
+    cout<<greatest;
+   
+    return 0;
+  }
