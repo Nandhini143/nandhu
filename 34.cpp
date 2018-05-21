cf428b951dc2b3b1d4ce2944e608f5ad4418e821
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-21 11:11:27 +0530

    Update 34.cpp

--- 34.cpp
+++ 34.cpp
@@ -1,13 +1,12 @@
-#include<stdio.h>
-#include<string.h>
+#include<iostream>
 int main()
 { 	char str[50];
 	int c=0,i;
-    scanf("%[^\n]s",str);
+  cin.getline(str, 50);
     int l;
     l=strlen(str);
 	for (i = 0; i<l; i++)
 		if (str[i] == '.')
 		c++;
-	printf("%d",c);
+	cout<<c;
 	return 0; }
