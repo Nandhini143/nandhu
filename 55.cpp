6c5fec72eb286b6386647e859931b90f110a3e58
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-22 10:36:20 +0530

    Create 55.cpp

---
+++ 55.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+int main()
+{
+int n,m,prod;
+cout<<"enter the first value";
+cin>>n;
+cout<<"enter the second value";
+cin>>m;
+prod=n*m;
+if(prod%2==0)
+{
+cout<<"even";
+}
+else
+{
+cout<<"odd";
+}
+return0;
+}
