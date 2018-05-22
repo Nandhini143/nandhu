378e31b60dec9cc775bc1b166508b67ffd99ae5e
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-22 10:08:56 +0530

    Create 50.cpp

---
+++ 50.cpp
@@ -0,0 +1,15 @@
+#include <iostream.h>
+            void main()
+            {
+            	int n=10;
+            	if(n%2==0)
+            	{
+     
+            cout<<"yes";
+     
+            	}
+            	else
+            	{
+            		cout<<"no";
+            	}
+            }
