c2acf0a182dbac487cb14b73c433f1b871174d2c
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-22 10:42:01 +0530

    Create 56.cpp

---
+++ 56.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(void) {
+	char a[100];
+	int alp=0,num=0;
+	cin>>a;
+	int i,m;
+	m=strlen(a);
+	for(i=0;i<m;i++)
+	{
+		if(('a'<=a[i] && a[i]<='z') || ('A'<=a[i] && a[i]<='Z'))
+		{
+			alp=1;
+		}
+		if('0'<=a[i] && a[i]<='9')
+		{
+			num=1;
+		}
+	}
+	if(num==1 && alp==1)
+	{
+		cout<<"yes";
+	}
+	else
+		cout<<"no";
+}
