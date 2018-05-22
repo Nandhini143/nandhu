4bdc96e05b02590db57b9ff0b0f2c08ca6269e84
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-22 10:04:39 +0530

    Create 49.cpp

---
+++ 49.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void) 
+{
+	int n1,n2,sum;
+	cout<<"\n enter the first number:";
+  cin>>n1;
+	cout<<"\n enter the second number:";
+	cin>>n2
+	sum=n1+n2;
+	cout<<sum;
+	return 0;
+}
