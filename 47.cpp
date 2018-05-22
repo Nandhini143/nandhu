895a1795c75f34f9d8ff9379ff83b0a6d62e2a2b
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-22 09:57:18 +0530

    Create 47.cpp

---
+++ 47.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+void main()
+{
+int a[50],i,n,min,max;
+cout<<"enter the no of elements";
+cin>>n;
+cout<<"enter array elements";
+for(i=0;i<n;i++)
+{
+cin.a[i];
+}
+min=a[0];
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]<min)
+{
+min=a[i];
+}
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+ cout<<"max = %d",max;
+   cout<<"\nmin = %d",min;
+}
