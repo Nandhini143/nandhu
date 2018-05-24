14d76d42d19a1f020f3a697c01b63dd13b506bde
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-24 10:57:41 +0530

    Create h2.cpp

---
+++ h2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[100],i,j,n,temp;
+
+	cin>>n;
+	
+	for(i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<"The biggest combination is..";
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		 if(a[i]<a[j])
+		 {
+		 	temp=a[i];
+		 	a[i]=a[j];
+		 	a[j]=temp;
+		 }
+		}
+	}
+		for(i=0;i<n;i++)
+		{
+			cout<<a[i];
+		}
+	return 0;
+}
