ff4429b15f11984fcaec384d94d9c6b15365f1d5
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-24 11:57:25 +0530

    Create h3.cpp

---
+++ h3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a[100],i,n;
+
+cin>>n;
+for(i=0;i<n;i++)
+   cin>>a[i];
+for(i=0;i<n;i++)
+
+{
+    if(a[i]==i)
+    {
+        cout<<i;
+        
+    }
+   
+
+}
+return 0;
+}
