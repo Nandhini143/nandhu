21117b1075462fada59a4d47c349493c02d8e31d
Nandhini143 <37546494+Nandhini143@users.noreply.github.com>
2018-05-25 10:41:01 +0530

    Create p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+ size_t maxlen = 0, leng;
+ int i;
+ int longest;
+ for (i = 1; i < argc; i++) 
+  {
+  len = strlen(argv[i]);
+  if (leng>maxlen) 
+  longest = argv[i];
+ }
+cout<<longest;
+return 0;
+}
